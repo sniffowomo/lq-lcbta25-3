9eddfdc00fb7a226197328522b7303b24f36187b
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-01-28 14:03:26 +0000

    🫦

--- WO/sec2/assCalc/Program.cs
+++ WO/sec2/assCalc/Program.cs
@@ -1,12 +1,16 @@
 ﻿// Main entry point 
 
+using Pastel;
+using System;
+using System.Drawing;
+
 namespace assCalc
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Create a new instance of the Calculator class
-            Calculator calc = new Calculator();
-
-            // Call the Run method of the Calculator class
+            Console.WriteLine("Welcome to assCalc!".Pastel(Color.FromArgb(255, 255, 0)));
+        }
+    }
+}
