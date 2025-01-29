50cf4fa1cfe708d38960682829441c52f9143ad0
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-01-29 12:40:51 +0000

    🫦

--- TEZT/sysacoms/Program.cs
+++ TEZT/sysacoms/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Syacomms running various sytem commands using chasrp 
+
+namespace sysacoms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Run the command
+            System.Diagnostics.Process.Start("cmd.exe", "/c ipconfig /all");
+        }
+    }
+}
