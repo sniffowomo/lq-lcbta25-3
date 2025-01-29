8787eb77f9a6b097e78df8c85ada6c4165ca7d22
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-01-29 12:10:37 +0000

    🫦

--- WO/sec2/works2/Program.cs
+++ WO/sec2/works2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Main entry point for the program
+
+using Pastel;
+using System.Drawing;
+
+namespace works2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Clear();
+            Console.WriteLine("Hello World!".Pastel(Color.Green));
+        }
+    }
+}
