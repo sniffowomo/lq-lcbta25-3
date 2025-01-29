18d60f88d13627be9eedaa6b2321686434a5a6b9
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-01-29 12:50:38 +0000

    🫦

--- TEZT/sysacoms/Program.cs
+++ TEZT/sysacoms/Program.cs
@@ -1,13 +1,18 @@
 ﻿// Syacomms running various sytem commands using chasrp 
 
+using Pastel;
+using System.Drawing;
+using sysacoms.src;
+
 namespace sysacoms
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Uti.PrintHeader("System Commands");
             // Run the command
-            System.Diagnostics.Process.Start("cmd.exe", "/c ipconfig /all");
+            // System.Diagnostics.Process.Start("ping", "www.google.com");
         }
     }
 }
