6fc94c8ce19b8abe3814f63e2993ef88507544ae
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2025-01-29 10:41:45 +0000

    🫦

--- WO/sec2/assCalc/src/Calc2.cs
+++ WO/sec2/assCalc/src/Calc2.cs
@@ -1,13 +1,22 @@
 // Calculator project from the tutorial 
 
+using Pastel;
+using System;
+using System.Drawing;
+
 namespace assCalc.src
 {
     public class Calc2
     {
         // Header function 
-        public static void Calc2Main()
+        public static void Calc2Header()
         {
             Uti.PrintHeader("Calculator 2 - Assignment");
+        }
+
+        public static void Calc2App()
+        {
+            Console.WriteLine("Enter the first number: ".Pastel(Color.AliceBlue));
         }
     }
 }
